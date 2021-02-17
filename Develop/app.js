9e606b4127a4699f8cf3fe59d1c2e45a3966153d
AMB <a.michael.boyle@gmail.com>
2021-02-16 19:41:41 -0500

    Got the prompts to output to team.html and show the cards

--- Develop/app.js
+++ Develop/app.js
@@ -4,44 +4,97 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+ 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+ 
 const render = require("./lib/htmlRenderer");
-
-
+const { create } = require("domain");
+ 
+ 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
+ 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
+ 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
+ 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-
+ 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
-
-const questions = [
-    {message: 'What is the team managers name?', name:'name'},
-    {message: 'What is the team managers id?', name:'id'},
-    
-]
-
-async function askQuestion(){
-    const response = await inquirer.prompt(questions)
-    const output = render(response)
+const employees = []
+ 
+async function createManager(){
+    const managerQuestions = [
+        {message: 'What is the team managers name?', name:'name'},
+        {message: 'What is the team managers id?', name:'id'},
+        {message: 'What is your team managers email?', name:'email'},
+        {message: 'What is the team managers office number?', name:'officeNumber'},
+    ]
+ 
+    const response = await inquirer.prompt(managerQuestions)
+    const manager = new Manager(response.name, response.id, response.email, response.officeNumber)
+    employees.push(manager)
+    fs.writeFileSync(outputPath, render(employees), 'UTF-8')
+    askEmployees();
 }
-askQuestion();
+createManager();
+ 
+async function askEmployees(){
+    const employeeType = await inquirer
+    .prompt( [
+        {type: 'list', message: 'What type of employee would you like to add?', name: 'employee', choices: ['Engineer', 'Intern', 'No employees']}
+    ])
+    
+    if(employeeType.employee === 'Engineer'){
+        console.log(employeeType)
+        createEngineer();
+    } 
+    if(employeeType.name === 'Intern') {
+        createIntern();
+    } 
+    if(employeeType.name === 'No employees') {
+        process.exit
+    }
+}
+ 
+// async function createEngineer(){
+//     const engineerQuestions = [
+//         {message: 'What is the team managers name?', name:'hi'},
+//         {message: 'What is the team managers id?', name:'bye'},
+//         {message: 'What is your team managers email?', name:'righj'},
+//         {message: 'What is the team managers office number?', name:'fdg'},
+//     ]
+    
+//     const response = await inquirer.prompt(engineerQuestions)
+//     const engineer = new Manager(response.name, response.id, response.email, response.officeNumber)
+//     employees.push(engineer)
+//     fs.writeFileSync(outputPath , render(employees), 'UTF-8')
+// }
+ 
+// async function createIntern(){
+//     const managerQuestions = [
+//         {message: 'What is the team managers name?', name:'name'},
+//         {message: 'What is the team managers id?', name:'id'},
+//         {message: 'What is your team managers email?', name:'email'},
+//         {message: 'What is the team managers office number?', name:'officeNumber'},
+//         {message: 'Which type of team member would you like to add?', name: 'employees'},
+//     ]
+    
+//     const response = await inquirer.prompt(questions)
+//     const manager = new Manager(response.name, response.id, response.email, response.officeNumber)
+//     employees.push(manager)
+//     fs.writeFileSync(outputPath , render(newManager), 'UTF-8')
+// }
