860fc69d580963bdd494a3745bfe6d7251638e81
AMB <a.michael.boyle@gmail.com>
2021-02-16 21:07:21 -0500

    Code clean up (moved explanation to the top), removed some whitespace

--- Develop/app.js
+++ Develop/app.js
@@ -1,17 +1,3 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
- 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
- 
-const render = require("./lib/htmlRenderer");
-const { create } = require("domain");
- 
- 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
  
@@ -34,6 +20,20 @@ const { create } = require("domain");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+ 
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+ 
+const render = require("./lib/htmlRenderer");
+const { create } = require("domain");
+ 
 const employees = []
  
 async function createManager(){
